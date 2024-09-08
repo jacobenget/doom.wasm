d685d94165f016276a595418c6877598ed242475
Jacob Enget <jacob.enget@gmail.com>
2024-09-08 18:09:08 -0500

    Provide a callback-free way of reporting key state
    Previously the user of this module had to be prepared to asynchronously
    report any key state changes, via implementing `ui.getKey`.
    
    This was awkward, likely relying on the user to manage their own
    cache of key states and only allowing one key state change to be
    reported during each call to `ui.getKey`.
    
    Instead, we'll provide a way for the user to report key state changes
    synchronously, via two new exported function `reportKeyDown` and
    `reportKeyUp`. Now the user can simply call these functions whenever
    they detect a change in key state, and the module will handle the rest.

--- src/doom_wasm.c
+++ src/doom_wasm.c
@@ -1,5 +1,7 @@
 #include "doomgeneric.h"
 
+#include <stdbool.h>
+
 /*
  * This file provides augmentations to the interface (i.e. imports and exports)
  * that is produced when Doom is compiled to a WebAssembly module.
@@ -11,8 +13,6 @@
 
 __attribute__((import_module("ui"))) void onGameInit(int width, int height);
 __attribute__((import_module("ui"))) void drawFrame(uint32_t *screenBuffer);
-__attribute__((import_module("ui"))) int getKey(int *pressed,
-                                                unsigned char *key);
 __attribute__((import_module("ui"))) void setWindowTitle(const char *title);
 
 __attribute__((import_module("runtimeControl"))) void sleepMs(uint32_t ms);
@@ -22,11 +22,24 @@ __attribute__((import_module("runtimeControl"))) uint32_t getTicksMs();
 // *                             EXPORTED FUNCTIONS                            *
 // *****************************************************************************
 
+// A global cache of which keys are currently pressed down, according to what
+// the user of this module has reported to via `reportKeyDown` and
+// `reportKeyUp`.
+static bool isKeyPressed[UINT8_MAX] = {false};
+
 __attribute__((visibility("default"))) void initGame(int argc, char **argv) {
   doomgeneric_Create(argc, argv);
 }
 
 __attribute__((visibility("default"))) void tickGame() { doomgeneric_Tick(); }
+
+__attribute__((visibility("default"))) void reportKeyDown(uint8_t doomKey) {
+  isKeyPressed[doomKey] = true;
+}
+
+__attribute__((visibility("default"))) void reportKeyUp(uint8_t doomKey) {
+  isKeyPressed[doomKey] = false;
+}
 
 // *****************************************************************************
 // *                           IMPLEMENTATION DETAILS                          *
@@ -39,7 +52,26 @@ void DG_Init() { onGameInit(DOOMGENERIC_RESX, DOOMGENERIC_RESY); }
 
 void DG_DrawFrame() { drawFrame(DG_ScreenBuffer); }
 
-int DG_GetKey(int *pressed, unsigned char *key) { return getKey(pressed, key); }
+int DG_GetKey(int *pressed, uint8_t *doomKey) {
+  // Keep a cache of what the last communicated state of each key was, so that
+  // each time the Doom engine calls this function we're prepared to relay any
+  // key state changes that have been reported since the last call to this
+  // function. By default all the keys start in the "not pressed" state.
+  static bool statePreviouslyCommunicated[UINT8_MAX] = {false};
+
+  for (int i = 0; i < UINT8_MAX; i++) {
+    // If the actual state of this key doesn't match the last communicated state
+    if (isKeyPressed[i] != statePreviouslyCommunicated[i]) {
+      *pressed = isKeyPressed[i];
+      *doomKey = i;
+      // We're communicating this key state change, so update the cache of the
+      // last communicated state of this key.
+      statePreviouslyCommunicated[i] = isKeyPressed[i];
+      return 1;
+    }
+  }
+  return 0;
+}
 
 void DG_SetWindowTitle(const char *title) { setWindowTitle(title); }
 
