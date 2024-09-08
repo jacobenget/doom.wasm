e4c8ab6cb91f26126ab95b304ef5cf8d4fe6b816
Jacob Enget <jacob.enget@gmail.com>
2024-09-08 17:36:44 -0500

    Have wasm imports be better organized and named
    Instead of blindly using the function names provided by doomgeneric,
    instead augment the names to provide the right amount of context and be
    better organized.
    
    This involves conceptually splitting the imports between a 'ui' module
    and a 'runtimeControl' module, which think will improve usability for
    users of the wasm module.

--- src/doom_wasm.c
+++ src/doom_wasm.c
@@ -5,8 +5,44 @@
  * that is produced when Doom is compiled to a WebAssembly module.
  */
 
+// *****************************************************************************
+// *                             IMPORTED FUNCTIONS                            *
+// *****************************************************************************
+
+__attribute__((import_module("ui"))) void onGameInit();
+__attribute__((import_module("ui"))) void drawFrame();
+__attribute__((import_module("ui"))) int getKey(int *pressed,
+                                                unsigned char *key);
+__attribute__((import_module("ui"))) void setWindowTitle(const char *title);
+
+__attribute__((import_module("runtimeControl"))) void sleepMs(uint32_t ms);
+__attribute__((import_module("runtimeControl"))) uint32_t getTicksMs();
+
+// *****************************************************************************
+// *                             EXPORTED FUNCTIONS                            *
+// *****************************************************************************
+
 __attribute__((visibility("default"))) void initGame(int argc, char **argv) {
   doomgeneric_Create(argc, argv);
 }
 
 __attribute__((visibility("default"))) void tickGame() { doomgeneric_Tick(); }
+
+// *****************************************************************************
+// *                           IMPLEMENTATION DETAILS                          *
+// *****************************************************************************
+
+// Here we implement the doomgeneric interface via the functions imported via
+// WebAssembly imports.
+
+void DG_Init() { onGameInit(); }
+
+void DG_DrawFrame() { drawFrame(); }
+
+int DG_GetKey(int *pressed, unsigned char *key) { return getKey(pressed, key); }
+
+void DG_SetWindowTitle(const char *title) { setWindowTitle(title); }
+
+void DG_SleepMs(uint32_t ms) { sleepMs(ms); }
+
+uint32_t DG_GetTicksMs() { return getTicksMs(); }
