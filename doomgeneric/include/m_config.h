73cd7708e3f8543a5d95242574ffc8481f7899f6
Jacob Enget <jacob.enget@gmail.com>
2024-09-17 17:42:52 -0500

    Remove many functions that are no longer needed
    Since the refactoring of how both WAD loading and game saving/loading
    works, many functions that were previously used to handle these tasks
    are no longer needed.
    
    This commit removes those functions (and some related variables), with
    the main goal of further reducing doom.wasm's dependency on WASI
    file system features.

--- doomgeneric/include/m_config.h
+++ doomgeneric/include/m_config.h
@@ -31,7 +31,6 @@ int M_GetIntVariable(char *name);
 const char *M_GetStrVariable(char *name);
 float M_GetFloatVariable(char *name);
 void M_SetConfigFilenames(char *main_config, char *extra_config);
-char *M_GetSaveGameDir(char *iwadname);
 
 extern char *configdir;
 
